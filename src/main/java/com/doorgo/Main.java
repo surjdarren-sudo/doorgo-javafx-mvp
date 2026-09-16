f76d2edbb7bacb68f76e1f2b527febc6e86f61f9
surjdarren-sudo <surj.darren@gmail.com>
2026-09-16 22:59:15 +0700

    Update Main.java

--- src/main/java/com/doorgo/Main.java
+++ src/main/java/com/doorgo/Main.java
@@ -7,18 +7,7 @@ import javafx.stage.Stage;
 
 import java.io.InputStream;
 
-/**
- * Main - titik masuk aplikasi DoorGo (JavaFX MVP).
- *
- * Sesuai modul OOP Bab 2.10: class ini WAJIB extends Application dan
- * meng-override start(Stage). Method main() hanya memanggil launch(args),
- * yang menyalakan runtime JavaFX lalu otomatis memanggil start().
- *
- * Catatan: proses multithreading (javafx.concurrent.Task) sekarang
- * ditaruh di LoginScreen dan RegisterScreen, karena di situlah aksi
- * yang berpotensi lambat (login/registrasi ke server) sebenarnya terjadi -
- * bukan saat aplikasi baru dibuka.
- */
+
 public class Main extends Application {
 
     @Override
@@ -32,11 +21,7 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-    /**
-     * stop() dipanggil otomatis oleh JavaFX saat jendela ditutup.
-     * Placeholder untuk cleanup resource (mis. menutup koneksi database
-     * begitu fitur JDBC ditambahkan di iterasi berikutnya).
-     */
+
     @Override
     public void stop() {
         System.out.println("DoorGo ditutup. (Tempat untuk cleanup resource nanti.)");
