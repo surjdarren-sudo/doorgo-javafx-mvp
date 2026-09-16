8a0e6994dd1672bdd6c5d313acf9979398714a5d
surjdarren-sudo <surj.darren@gmail.com>
2026-09-16 23:02:36 +0700

    Update Screen.java

--- src/main/java/com/doorgo/view/Screen.java
+++ src/main/java/com/doorgo/view/Screen.java
@@ -3,18 +3,7 @@ package com.doorgo.view;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- * Screen - "cetak biru" untuk setiap layar/halaman aplikasi DoorGo.
- *
- * Menerapkan ABSTRACTION: kelas ini tidak tahu (dan tidak peduli) detail
- * tampilan tiap halaman, ia hanya mewajibkan setiap turunannya (subclass)
- * menyediakan method build() yang mengembalikan Scene siap-pakai.
- *
- * LoginScreen dan HomeScreen adalah contoh INHERITANCE: keduanya
- * extends Screen dan mewarisi field `stage`, lalu meng-override build()
- * dengan caranya masing-masing -> ini juga contoh POLYMORPHISM,
- * sama seperti method render() pada GameObject/Player/Enemy di modul OOP.
- */
+
 public abstract class Screen {
 
     protected final Stage stage;
